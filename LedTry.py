9f82d4167991b0ec965da7b245ad419cd3bc5322
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-18 13:37:11 +0300

    LedTry

---
+++ LedTry.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+sleeptime = .1
+
+ledPin = 23
+
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.output(ledPin, False)
+
+try:
+    while True:
+        GPIO.output(ledPin, True)
+        sleep(sleeptime)
+finally:
+    GPIO.output(ledPin, False)
+    GPIO.cleanup()
