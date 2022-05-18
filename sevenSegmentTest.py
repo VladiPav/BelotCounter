0a1a3ffa44ca78b0b38c026080fd63f8f51dfd76
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-18 17:42:18 +0300

    trying7Segment

--- sevenSegmentTest.py
+++ sevenSegmentTest.py
@@ -8,15 +8,15 @@ segments = (21, 20, 26, 19, 16, 13, 6)
 
 for i in segments:
     GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, False)
+    GPIO.output(i, True)
 
 try:
     while True:
         for i in segments:
-            GPIO.output(i, True)
+            GPIO.output(i, False)
         sleep(sleeptime)
         
 finally:
     for i in segments:
-        GPIO.output(i, False)
+        GPIO.output(i, True)
     GPIO.cleanup()
