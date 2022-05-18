1f910c85bbc8f3494b2b87deb13ce44ed58cfa2b
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-18 17:49:37 +0300

    trying7Segment

--- sevenSegmentTest.py
+++ sevenSegmentTest.py
@@ -3,16 +3,26 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
+
 sleeptime = .1
 segments = (21, 20, 26, 19, 16, 13, 6)
+
+common = (2, 3, 3)
 
 for i in segments:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, True)
 
+for i in common:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, False)
+
 try:
     while True:
-        for i in segments:
+        for i in common:
+            GPIO.output(i, True)
+            for j in segments:
+                GPIO.output(j, False)
             GPIO.output(i, False)
         sleep(sleeptime)
         
