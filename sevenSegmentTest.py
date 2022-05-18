53d60fc4f02556234fea4782fab2ff235b5d6ecf
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-18 19:36:27 +0300

    trying7Segment

--- sevenSegmentTest.py
+++ sevenSegmentTest.py
@@ -7,7 +7,20 @@ GPIO.setmode(GPIO.BCM)
 sleeptime = .001
 segments = (21, 20, 26, 19, 16, 13, 6)
 
-common = (2, 3, 4)
+common = (2, 3, 17)
+
+digits = (
+    (0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 1, 1, 1, 1),
+    (0, 0, 1, 0, 0, 1, 0),
+    (0, 0, 0, 0, 1, 1, 0),
+    (1, 0, 0, 1, 1, 0, 0),
+    (0, 1, 0, 0, 1, 0, 0),
+    (0, 1, 0, 0, 0, 0, 0),
+    (0, 0, 0, 1, 1, 1, 1),
+    (0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 0, 1, 0, 0)
+)
 
 for i in segments:
     GPIO.setup(i, GPIO.OUT)
@@ -17,16 +30,19 @@ for i in common:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, False)
 
+
+
 try:
     while True:
         for i in common:
             GPIO.output(i, True)
             for j in segments:
-                GPIO.output(j, False)
-            GPIO.output(i, False)
+                GPIO.output(j, digits[2][j])
         sleep(sleeptime)
         
 finally:
     for i in segments:
         GPIO.output(i, True)
+    for i in common:
+        GPIO.output(i, False)
     GPIO.cleanup()
