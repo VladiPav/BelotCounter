ffde7e8530dff09fddffc295f393b7c115527804
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 20:18:02 +0300

    relayTest

--- relayTest.py
+++ relayTest.py
@@ -13,8 +13,10 @@ GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.output(relayPin, False)
 try:
     while True:
-        GPIO.output(relayPin, not GPIO.input(btnPin))
-        sleep(sleeptime)
+        GPIO.output(relayPin, True)
+        sleep(1)
+        GPIO.output(relayPin, False)
+        sleep(1)
 finally:
     GPIO.output(relayPin, False)
     GPIO.cleanup()
