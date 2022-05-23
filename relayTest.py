fa805de7ac328dfd9746e67479f29aa86831b565
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 20:26:11 +0300

    relayTest

--- relayTest.py
+++ relayTest.py
@@ -1,21 +1,38 @@
-import time
-
 import RPi.GPIO as GPIO
-
-relayPin = 14
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
+
+blinkCount = 3
+count = 0
+relayPin = 14
+buttonPin = 5
+
+# Setup the pin the LED is connected to
 GPIO.setup(relayPin, GPIO.OUT)
+# Setup the button
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-time.sleep(1)
-GPIO.output(relayPin, GPIO.LOW)
+buttonPress = True
+relayState = False
 
-time.sleep(1)
-
-time.sleep(1)
-GPIO.output(relayPin, GPIO.LOW)
-
-time.sleep(1)
-
-time.sleep(1)
-GPIO.cleanup()
+try:
+    while count < blinkCount:
+        print("Come on man, press the button!")
+        buttonPress = GPIO.input(buttonPin)
+        if buttonPress == False and relayState == False:
+            GPIO.output(relayPin, True)
+            print("relay ON")
+            relayState = True
+            sleep(0.5)
+        elif buttonPress == False and relayState == True:
+            GPIO.output(relayPin, False)
+            print("relay OFF")
+            relayState = False
+            count += 1
+            sleep(0.5)
+        sleep(0.1)
+finally:
+    # Reset the GPIO Pins to a safe state
+    GPIO.output(relayPin, False)
+    GPIO.cleanup()
