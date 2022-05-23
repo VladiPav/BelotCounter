f494225ff18d65e0c463f9063f0ef18dd74c2693
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 21:42:39 +0300

    suitCheckMain

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
+
 import numpy as np
 import cv2 
 import os
 from VideoStream import VideoStream
 
-path = 'C:\\Users\\vladi\\OneDrive\\School\\OpenCV\\BelotCounter\\MyCardImages\\SuitImages'
-SuitImages = []
-RankImages = []
+def is_template_in_image(img, template):
+
+    result = cv2.matchTemplate(img, template, cv2.TM_SQDIFF)
+
+    min_val = cv2.minMaxLoc(result)[0]
+
+    thr = 10e-6
+
+    return min_val <= thr
+
+
+path = 'C:\\Users\\vladi\\OneDrive\\School\\OpenCV\\BelotCounter\\MyCardImages'
+suitImages = []
+rankImages = []
 suits = []
 ranks = []
 for suit in os.listdir(path + "/SuitImages"):
     imgCur = cv2.imread(f'{path}/SuitImages/{suit}', 0)
-    SuitImages.append(imgCur)
+    suitImages.append(imgCur)
     suits.append(os.path.splitext(suit)[0])
 
 for rank in os.listdir(path + "/RankImages"):
     imgCur = cv2.imread(f'{path}/RankImages/{suit}', 0)
-    RankImages.append(imgCur)
+    rankImages.append(imgCur)
     ranks.append(os.path.splitext(rank)[0])
 
 
 video = VideoStream(0).start()
 
 f = set()
-
 while True:
-    print(f)
-    im = video.read()
+    currSuit = "Unknown"
+    img = video.read()
     threshold = 0.87
-    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     i = 0
-    counter = 0
-    while i < len(RankImages):
-        template = RankImages[i]
-        
+    j = 0
+    while j < len(suitImages):
+        template = suitImages[j]
+        if(is_template_in_image(img, template)):
+            currSuit = suits[j]
+    print(currSuit)
+    cv2.imshow("img", img)
+            
     """while i < len(images):
         template = images[i]
         w, h = template.shape[::-1]
