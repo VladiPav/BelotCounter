f54bc476938387919334f577f83ba82f0616e4de
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 23:31:17 +0300

    whiteLED

--- buttonTest.py
+++ buttonTest.py
@@ -5,11 +5,11 @@ GPIO.setmode(GPIO.BCM)
 
 sleeptime = .1
 
-ledPin = 23
-btnPin = 18
+ledPin = 12
+btnPin = 5
 
 GPIO.setup(ledPin, GPIO.OUT)
-GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.output(ledPin, False)
 try:
     while True:
