d8e08c77d6991b5af158a1128e21fa653c85dac3
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 20:06:13 +0300

    relayTest

--- relayTest.py
+++ relayTest.py
@@ -1,22 +1,14 @@
+import time
 
 import RPi.GPIO as GPIO
-from time import sleep
-
-GPIO.setmode(GPIO.BCM)
-
-sleeptime = .1
 
 relayPin = 14
-btnPin = 5
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(relayPin, GPIO.OUT)
-GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.output(relayPin, GPIO.LOW)
-try:
-    while True:
-        GPIO.output(relayPin, GPIO.LOW)
-        sleep(2)
-        GPIO.output(relayPin, GPIO.HIGH)
-finally:
-    GPIO.output(relayPin, GPIO.LOW)
-    GPIO.cleanup()
+GPIO.output(relayPin, False)
+
+time.sleep(2)
+
+GPIO.output(relayPin, True)
+GPIO.cleanup()
