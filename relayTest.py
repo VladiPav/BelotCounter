92e48dc8777421cfdef807f8611f9a3e7ace9522
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-05-23 20:24:43 +0300

    relayTest

--- relayTest.py
+++ relayTest.py
@@ -6,19 +6,16 @@ relayPin = 14
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relayPin, GPIO.OUT)
-GPIO.output(relayPin, GPIO.HIGH)
 
 time.sleep(1)
 GPIO.output(relayPin, GPIO.LOW)
 
 time.sleep(1)
-GPIO.output(relayPin, GPIO.HIGH)
 
 time.sleep(1)
 GPIO.output(relayPin, GPIO.LOW)
 
 time.sleep(1)
-GPIO.output(relayPin, GPIO.HIGH)
 
 time.sleep(1)
 GPIO.cleanup()
